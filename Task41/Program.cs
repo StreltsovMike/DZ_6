69788e2909615217ab707d6402dd3254506cd5dd
Streltsov_Mike <streltsovmike@yandex.ru>
2022-07-13 19:14:31 +0300

    Add Homework

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+int PositivNumberCounter(int arraySize)
+{
+    int[] array = new int[arraySize];
+    int positivCount = 0;
+    for (int count = 0; count < arraySize; count++)
+    {
+        Console.WriteLine("Введите число : ");
+        array[count] = Convert.ToInt32(Console.ReadLine());
+        if(array[count]>0)
+        {
+            positivCount++;
+        }
+    }
+    return positivCount;
+}
+
+Console.WriteLine("Введите количество чисел : ");
+int amountOfNumbers= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Положительных - {PositivNumberCounter(amountOfNumbers)}");
